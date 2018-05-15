25c6555220a8ada9c0b47fe3a859f0eceabc9155
Unknown <jacques7473@gmail.com>
2018-05-15 11:57:50 +0200

    Auto stash before merge of "greg" and "origin/greg"

--- lib/header.h
+++ lib/header.h
@@ -1,3 +1,4 @@
+ 
 
 typedef struct l{
   char nom;
--- src/ProjetPLS_nbOccurences.c
+++ src/ProjetPLS_nbOccurences.c
@@ -2,29 +2,12 @@
 #include <stdio.h>
 #include "header.h"
 
-tab_occur nbOccurences(char *fichier){
-  tab_occur tab;
-  tab.nbOccurences=0;
-  for(int i=0 ; i<256 ; i++){
-    tab.tab[i] = 0;
-  }
-  // Tableau a initialiser et mettre à 0
+void nbOccurences(char *fichier, char **nbOcc, int *nbSymbole){
   FILE* f = NULL;
-  //lettre récupérée dans le fichier texte
-  char lettre;
-  //nombre total de symbols dans le fichier txt
-  int nbSymbole=0;
   f = fopen(fichier,"r");
   if (f != NULL)
   {
-    fscanf(f, "%c", &lettre);
-    tab.tab[lettre]++;
-    tab.nbOccurences++;
-    while(!feof(f)){
-      fscanf(f, "%c", &lettre);
-      tab.tab[lettre]++;
-      tab.nbOccurences++;
-    }
+
   }
   else
   {
@@ -32,42 +15,4 @@ tab_occur nbOccurences(char *fichier){
     printf("Impossible d'ouvrir le fichier test.txt");
   }
   return tab;
-}
-
-pliste_t triTable(tab_occur tab){
-  pliste_t liste=NULL ;
-  pliste_t first;
-  for(int i=0;i<256;i++){
-    if(tab.tab[i]!=0){
-      first=malloc(sizeof(pliste_t));
-      first->nom=i;
-      first->nb=tab.tab[i];
-      first->next=NULL;
-      if(liste==NULL){
-        liste=first;
-      }else{
-        pliste_t test=liste;
-        if(liste->nb>first->nb){
-          first->next=liste;
-          liste=first;
-        }else{
-        while(test->next!=NULL && test->next->nb<=first->nb){
-          test=test->next;
-        }
-        first->next=test->next;
-        test->next=first;
-      }}
-    }
-  }
-  return liste;
-}
-
-int main(){
-  tab_occur tri =nbOccurences("../exemple/exem1.txt");
-  pliste_t liste=triTable(tri);
-  while(liste!=NULL){
-    printf("%c : %d\n",liste->nom,liste->nb );
-    liste=liste->next;
-  }
-  return 0;
 }
