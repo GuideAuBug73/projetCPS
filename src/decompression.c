3896df76aef7cfb1edb6760647ee8354554d7510
Unknown <solverytom@hotmail.fr>
2018-05-17 09:03:41 +0200

    Creation message à partir d'arbre et tableau binaire

--- src/decompression.c
+++ src/decompression.c
@@ -4,7 +4,7 @@
 #include <string.h>
 #include "decompression.h"
 #include "huffman.h"
-#define max(a,b) ((a)>(b)?(a):(b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
 
 /*char **createArray(int m, int n) {
     char **rows = malloc(m * sizeof(char *));
@@ -15,33 +15,39 @@
     return rows;
 }
 */
-enTete_t lectureTableLongueur(char *fichier){
+enTete_t lectureTableLongueur(char *fichier)
+{
   enTete_t entete;
   FILE *f = NULL;
   int longueur = -1;
   int profondeur = 0;
   //longueur inferieur a 10
-  int *NbSymb=malloc(9*sizeof(int));
-  char **Symb=createArray(10, 512);
-  for(int i = 0 ; i<9 ; i++){
-    NbSymb[i]=0;
+  int *NbSymb = malloc(9 * sizeof(int));
+  char **Symb = createArray(10, 512);
+  for (int i = 0; i < 9; i++)
+  {
+    NbSymb[i] = 0;
   }
   entete.NbSymb = NbSymb;
-  f = fopen(fichier,"r");
+  f = fopen(fichier, "r");
   //calcul premier tableau tab[0] correspond au niveau 1
-  if(f!=NULL){
+  if (f != NULL)
+  {
     //calcul profondeur + premier tableau
     profondeur = longueur;
-    fscanf(f,"%d",&longueur);
-    if(longueur!=0){
+    fscanf(f, "%d", &longueur);
+    if (longueur != 0)
+    {
       entete.NbSymb[longueur]++;
       profondeur = max(profondeur, longueur);
     }
-    while(!feof(f)){
-      fscanf(f,"%d",&longueur);
-      if(longueur!=0){
+    while (!feof(f))
+    {
+      fscanf(f, "%d", &longueur);
+      if (longueur != 0)
+      {
         profondeur = max(profondeur, longueur);
-        if(longueur != -1)
+        if (longueur != -1)
           entete.NbSymb[longueur]++;
         longueur = -1;
       }
@@ -50,20 +56,26 @@ enTete_t lectureTableLongueur(char *fichier){
     fclose(f);
 
     //calcul deuxieme tableau
-    f = fopen(fichier,"r");
-    int tete[256];for(int i=0 ; i<256 ; i++){
-      tete[i]=0;
+    f = fopen(fichier, "r");
+    int tete[256];
+    for (int i = 0; i < 256; i++)
+    {
+      tete[i] = 0;
     }
-    for(int i=0 ; i<256 ; i++){
-      fscanf(f,"%d",&longueur);
-      tete[i]=longueur;
+    for (int i = 0; i < 256; i++)
+    {
+      fscanf(f, "%d", &longueur);
+      tete[i] = longueur;
     }
     fclose(f);
     //f = fopen(fichier,"r");
-    for(int i=1 ; i<=entete.profondeur ; i++){
-      int k=0;
-      for(int j=0 ; j<256 ; j++){
-        if(tete[j] == i){
+    for (int i = 1; i <= entete.profondeur; i++)
+    {
+      int k = 0;
+      for (int j = 0; j < 256; j++)
+      {
+        if (tete[j] == i)
+        {
           //printf("i : %d k : %d j : %d\n",i,k,j);
           Symb[i][k] = j;
           k++;
@@ -73,10 +85,34 @@ enTete_t lectureTableLongueur(char *fichier){
     entete.Symb = Symb;
     //fclose(f);
   }
-  else printf("Impossible d'ouvrir le fichier test.txt");
+  else
+    printf("Impossible d'ouvrir le fichier test.txt");
   return entete;
 }
 
+char *decompression_final(char *tab, arbre a, int nbmots)
+{
+  pnoeud x = a;
+  char *message = malloc(sizeof(char)*nbmots);
+  int k = 0;
+  for (int i = 0; i < nbmots; i++)
+  {
+    while (x->gauche != NULL && x->droit != NULL)
+    {
+      if (tab[k] == '0')
+      {
+        x = x->droit;
+      }
+      else if (tab[k] == '1'){
+        x = x->gauche;
+      }
+      k++;
+    }
+    message[i] = x->s;
+    x = a;
+  }
+  return message;
+}
 /*
 int main(int argc, char *argv){
   enTete_t entete;
